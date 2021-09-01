69ee6e4908adc4725d478b8a2822d1fec3d913e2
Elena Pascal <elenadpascal@gmail.com>
2021-09-01 16:24:36 +0100

    Greet users.

--- crysthack_day1_GitHub_workflow_exercise/src/main.py
+++ crysthack_day1_GitHub_workflow_exercise/src/main.py
@@ -1,6 +1,26 @@
+import glob
+import random
 
-def main():
-    print("ok, well that's a start")
+
+def read_names():
+    """
+    Read the names in ../names
+    :return: names_list
+    """
+    names_list = []
+    for name_file in glob.glob("./names/*"):
+        with open(name_file, 'r') as f:
+            first_line = f.readline()
+            first_name = first_line.split()[0]
+            names_list.append(first_name)
+    return names_list
+
+
+def greet(names):
+    greeting_list = ["Hello", "Hi", "Howdy"]
+    print("%s, %s!" % (random.choice(greeting_list),
+                       random.choice(names)))
+
 
 if __name__ == '__main__':
-    main()
+    greet(read_names())
