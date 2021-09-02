6a536f1e92753428d9bd62c00b920998bcd7018d
rowlesmr <rowlesmr@gmail.com>
2021-09-02 22:40:40 +0800

    many updates to main.py
    Removed the type of the variable from its name. eg name_list --> names
    Corrected bug in getting to the names directory
    made greetings global, so it can be used by multiple functions
    Add more greetings
    Added more greeting functions
    moved the main function calls into a function called main()

--- crysthack_day1_GitHub_workflow_exercise/src/main.py
+++ crysthack_day1_GitHub_workflow_exercise/src/main.py
@@ -5,22 +5,46 @@ import random
 def read_names():
     """
     Read the names in ../names
-    :return: names_list
+    :return: list of names
     """
-    names_list = []
-    for name_file in glob.glob("./names/*"):
+    names = []
+    for name_file in glob.glob("../names/*"):
         with open(name_file, 'r') as f:
             first_line = f.readline()
             first_name = first_line.split()[0]
-            names_list.append(first_name)
-    return names_list
+            names.append(first_name)
+    return names
 
 
-def greet(names):
-    greeting_list = ["Hello", "Hi", "Howdy","Hola","Ciao"]
-    print("%s, %s!" % (random.choice(greeting_list),
-                       random.choice(names)))
+greetings = ["Hello", "Hi", "Howdy", "Hola", "Ciao", "G'day", "What's up", "Howzgarn"]
+
+
+def greet_random(names):
+    global greetings
+    print(f"{random.choice(greetings)}, {random.choice(names)}!")
+
+
+def greet_all(names):
+    global greetings
+    for name in names:
+        print(f"{random.choice(greetings)}, {name}!")
+
+
+def greet_each_other(names):
+    global greetings
+    name1, name2 = random.sample(names, 2)
+
+    print(f"{random.choice(greetings)}, my name is {name1}, what's your name?")
+    print(f"{random.choice(greetings)}, my name is {name2}, How are you enjoying this course?")
+    print("I am loving it, but I wish the conference was in-person and we could all be there...")
+
+
+def main():
+    names = read_names()
+    greet_random(names)
+    greet_all(names)
+    greet_each_other(names)
 
 
 if __name__ == '__main__':
-    greet(read_names())
+    main()
